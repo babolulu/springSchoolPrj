eac06bf313b80cfbb3983aa165c089aedf6ead8f
Jiman Kim <babolulu@hanmail.net>
2016-04-24 23:33:45 +0900

    beginning

---
+++ src/main/java/com/member/school/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.member.school.service;
+
+import com.member.school.model.Student;
+
+public interface StudentService {
+	void insertStudent(Student student);
+	boolean getStudentByLogin(String userName, String password);
+	boolean getStudentByUserName(String userName);
+}
