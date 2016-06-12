4352f109bfabdfb2366ebf122422e72e5d5b2df2
Jiman Kim <babolulu@hanmail.net>
2016-06-12 11:27:31 +0900

    original

--- src/main/java/com/member/school/service/StudentServiceImpl.java
+++ src/main/java/com/member/school/service/StudentServiceImpl.java
@@ -1,10 +1,6 @@
 package com.member.school.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,28 +12,10 @@ public class StudentServiceImpl implements StudentService {
 
 	@Autowired
 	private SchoolMemberMapper schoolMemberMapper;
-	
-	@Value("2. set Method 수행")
-	private String message;
-	
-	public StudentServiceImpl(){
-		System.out.println("1. StudentServiceImpl 생성자 메서드 수행");
-	}
-	
+		
 	@Transactional
 	public void insertStudent(Student student) {
 		schoolMemberMapper.insertStudent(student);
-	}
-
-	@PostConstruct
-	public void initMethod(){
-		System.out.println("2." + message);
-		System.out.println("3. init method invoked");
-	}
-	
-	@PreDestroy
-	public void destroyMethod(){
-		System.out.println("4. destroy method invoked");
 	}
 	
 	public boolean getStudentByLogin(String emailAddress, String password) {
